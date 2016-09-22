f0517f984ee047ea59e9fff1ae1f9297b86a6c2e
Alex Chesser <alex.chesser@gmail.com>
2016-09-22 16:26:35 -0400

    setup obvious parts of testing file.

---
+++ 04-travellingsalesman/cities_test.py
@@ -0,0 +1,21 @@
+import unittest
+from cities import *
+
+class TestCities(unittest.TestCase):
+    def test_read_cities(self):
+        self.fail()
+
+    def test_compute_total_distance(self):
+        self.fail()
+
+    def test_swap_adjacent_cities(self):
+        self.fail()
+
+    def test_swap_cities(self):
+        self.fail()
+
+    def test_find_best_cycle(self):
+        self.fail()
+
+
+unittest.main()
